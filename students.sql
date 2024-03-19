179f9bfee7de8224d91fd952840edc71f796aa90
zyj1218 <139957391+zyj1218@users.noreply.github.com>
2024-03-19 00:03:48 -0400

    Add files via upload

---
+++ students.sql
@@ -0,0 +1,12 @@
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    enrollment_date DATE
+);
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
